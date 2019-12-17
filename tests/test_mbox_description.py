62d58a0b078877d63267078d8a738d69ad08101f
Changqing Li <changqing.li@windriver.com>
2019-12-17 14:29:31 +0800

    Add SPDX-License-Identifier: GPL-2.0-or-later to source files
    [YOCTO #13532]
    
    Signed-off-by: Changqing Li <changqing.li@windriver.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- tests/test_mbox_description.py
+++ tests/test_mbox_description.py
@@ -15,6 +15,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 import base
 
 class CommitMessage(base.Base):
